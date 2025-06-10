6b1a2401e5a74cdfd14089d6ea929758178ff65b
Md Hasnath Karim <hasnath.karim@gmail.com>
2025-06-10 14:38:27 +0100

    folder structure created

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       
-      <h1 className='text-5xl'>Vite + React</h1>
+      <h1 className='text-5xl'>BulkCartel</h1>
       <button className="btn btn-accent">Accent</button>
     
      
